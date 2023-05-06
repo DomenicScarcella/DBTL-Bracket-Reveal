4ce0d7c326685f9c07331af27f865d3b77cd6f4f
unknown <ls41513@yahoo.com>
2023-05-05 22:49:30 -0400

    Move photos of wrestlers to art_common

--- src/Bracket/dataBracket.js
+++ src/Bracket/dataBracket.js
@@ -1,23 +1,23 @@
-import * as bp from "./bracketphotos/bp-index.js";
+import * as p from "../art_common/wrestlerphotos/0-index.js";
 
-const a1 = {name: "1. Roman Reigns", ph: bp.ROMAN, see: "hide"};
-const a2 = {name: "2. Gunther", ph: bp.GUNTHER, see: "hide"};
-const a3 = {name: "3. Austin Theory", ph: bp.THEORY, see: "hide"};
-const a4 = {name: "4. Kevin Owens", ph: bp.KO, see: "hide"};
-const a5 = {name: "5. Sami Zayn", ph: bp.SAMI, see: "hide"};
-const a6 = {name: "6. Brock Lesnar", ph: bp.BROCK, see: "hide"};
-const a7 = {name: "7. Cody Rhodes", ph: bp.CODY, see: "hide"};
-const a8 = {name: "8. Bobby Lashley", ph: bp.LASHLEY, see: "hide"};
+const a1 = {name: "1. Roman Reigns", ph: p.ROMAN, see: "hide"};
+const a2 = {name: "2. Gunther", ph: p.GUNTHER, see: "hide"};
+const a3 = {name: "3. Austin Theory", ph: p.THEORY, see: "hide"};
+const a4 = {name: "4. Kevin Owens", ph: p.KO, see: "hide"};
+const a5 = {name: "5. Sami Zayn", ph: p.SAMI, see: "hide"};
+const a6 = {name: "6. Brock Lesnar", ph: p.BROCK, see: "hide"};
+const a7 = {name: "7. Cody Rhodes", ph: p.CODY, see: "hide"};
+const a8 = {name: "8. Bobby Lashley", ph: p.LASHLEY, see: "hide"};
 
-const b1 = {name: "1. Roman Reigns", ph: bp.ROMAN, see: "hide"};
-const b2 = {name: "2. Gunther", ph: bp.GUNTHER, see: "hide"};
-const b3 = {name: "3. Austin Theory", ph: bp.THEORY, see: "hide"};
-const b4 = {name: "4. Kevin Owens", ph: bp.KO, see: "hide"};
+const b1 = {name: "1. Roman Reigns", ph: p.ROMAN, see: "hide"};
+const b2 = {name: "2. Gunther", ph: p.GUNTHER, see: "hide"};
+const b3 = {name: "3. Austin Theory", ph: p.THEORY, see: "hide"};
+const b4 = {name: "4. Kevin Owens", ph: p.KO, see: "hide"};
 
-const c1 = {name: "1. Roman Reigns", ph: bp.ROMAN, see: "hide"};
-const c2 = {name: "2. Gunther", ph: bp.GUNTHER, see: "hide"};
+const c1 = {name: "1. Roman Reigns", ph: p.ROMAN, see: "hide"};
+const c2 = {name: "2. Gunther", ph: p.GUNTHER, see: "hide"};
 
-export const champ = {name: "1. Roman Reigns", ph: bp.ROMAN, see: "hide"};
+export const champ = {name: "1. Roman Reigns", ph: p.ROMAN, see: "hide"};
 
 export const Rd1L = [a1, a8, a4, a5];
 export const Rd1R = [a2, a7, a3, a6];
