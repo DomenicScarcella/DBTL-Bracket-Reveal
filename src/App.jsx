d31de9ac95ecc83620c696662560d926e1dd3a05
unknown <ls41513@yahoo.com>
2023-05-21 14:21:12 -0400

    What we used for the podcast!

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,7 @@
-// import { useState } from "react";
 import { BracketModular } from "./Bracket/BracketModular";
 import "./App.css";
 
 export function App() {
-  // const [head, setHead] = useState("RECAP");
-
-  // function NavHead() {
-  //   return (
-  //     <div>
-  //       <form>
-  //         <input
-  //           type="radio"
-  //           value="RECAP"
-  //           id="RECAP"
-  //           onChange={() => {
-  //             setHead("RECAP")
-  //           }} 
-  //         />
-  //         <label>Recap</label>
-  //         <input
-  //           type="radio"
-  //           value="PREVIEW"
-  //           id="PREVIEW"
-  //           onChange={() => {
-  //             setHead("PREVIEW")
-  //           }} 
-  //         />
-  //         <label>Preview</label>
-  //         <input
-  //           type="radio"
-  //           value="SPECIAL"
-  //           id="SPECIAL"
-  //           onChange={() => {
-  //             setHead("SPECIAL")
-  //           }} 
-  //         />
-  //         <label>Special</label>
-  //       </form>
-  //     </div>
-  //   )
-  // }
-
-
-
-
   return (
     <div>
       <div className="header">
@@ -54,12 +12,7 @@ export function App() {
         <BracketModular />
       </div>
       <div className="footer">
-        {/* <h1>WWE SmackDown - May 12, 2023</h1> */}
         <h1>Who Would Win a Real Fight?</h1>
-        {/* <div className="navblock">
-          <NavHead />
-
-        </div> */}
       </div>
     </div>
   )
--- src/Bracket/dataBracket.js
+++ src/Bracket/dataBracket.js
@@ -9,15 +9,15 @@ const a6 = {name: "6. Brock Lesnar", ph: p.BROCK, see: "hide"};
 const a7 = {name: "7. Cody Rhodes", ph: p.CODY, see: "hide"};
 const a8 = {name: "8. Bobby Lashley", ph: p.LASHLEY, see: "hide"};
 
-const b1 = {name: "1. Roman Reigns", ph: p.ROMAN, see: "hide"};
-const b2 = {name: "2. Gunther", ph: p.GUNTHER, see: "hide"};
-const b3 = {name: "3. Austin Theory", ph: p.THEORY, see: "hide"};
-const b4 = {name: "4. Kevin Owens", ph: p.KO, see: "hide"};
+const b1 = {name: "8. Bobby Lashley", ph: p.LASHLEY, see: "hide"};
+const b2 = {name: "7. Cody Rhodes", ph: p.CODY, see: "hide"};
+const b3 = {name: "6. Brock Lesnar", ph: p.BROCK, see: "hide"};
+const b4 = {name: "5. Sami Zayn", ph: p.SAMI, see: "hide"};
 
-const c1 = {name: "1. Roman Reigns", ph: p.ROMAN, see: "hide"};
-const c2 = {name: "2. Gunther", ph: p.GUNTHER, see: "hide"};
+const c1 = {name: "8. Bobby Lashley", ph: p.LASHLEY, see: "hide"};
+const c2 = {name: "6. Brock Lesnar", ph: p.BROCK, see: "hide"};
 
-export const champ = {name: "1. Roman Reigns", ph: p.ROMAN, see: "hide"};
+export const champ = {name: "6. Brock Lesnar", ph: p.BROCK, see: "hide"};
 
 export const Rd1L = [a1, a8, a4, a5];
 export const Rd1R = [a2, a7, a3, a6];
